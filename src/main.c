cc8c14ed6d45b07aed3296e288c0cc465fb81e7c
migmanu <jmanuelmigoya@gmail.com>
2023-11-04 16:48:49 +0100

    Add arg coun check

--- src/main.c
+++ src/main.c
@@ -6,14 +6,19 @@
 /*   By: migmanu <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 15:50:52 by migmanu           #+#    #+#             */
-/*   Updated: 2023/11/04 15:51:20 by migmanu          ###   ########.fr       */
+/*   Updated: 2023/11/04 16:44:11 by migmanu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-int	main(void)
+int	main(int argc, char *argv[])
 {
+	if (argc != 1)
+	{
+		printf("minishell takes no arguments, you passed %s\n", argv[1]);
+		exit(ERROR);
+	}
 	init_promt("USER");
 	return (0);
 }
