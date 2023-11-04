fd7a8618493f2613cc39db58de55c2d0fe2271e6
migmanu <jmanuelmigoya@gmail.com>
2023-11-04 15:48:12 +0100

    Testing

--- src/testing/readline_testing.c
+++ src/testing/readline_testing.c
@@ -6,23 +6,37 @@
 /*   By: migmanu <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 14:21:59 by migmanu           #+#    #+#             */
-/*   Updated: 2023/11/04 15:15:50 by migmanu          ###   ########.fr       */
+/*   Updated: 2023/11/04 15:47:44 by migmanu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-int	main(void)
+void	init_promt(char *str)
 {
-	char	*hostname;
+	char	*symbol;
+	char	*var;
 	char	*promt;
 	char	*line;
 
-	hostname = getenv("HOSTNAME");
-	promt = "_> ";
+	symbol = " $_> ";
+	var = getenv(str);
+	if (var)
+	{
+		promt = ft_strjoin(var, symbol);
+	}
+	else
+		promt = symbol;
 	while (1)
 	{
 		line = readline(promt);
 		add_history(line);
 	}
+	free(promt);
+}
+
+int	main(void)
+{
+	init_promt("USER");
+	return (0);
 }
