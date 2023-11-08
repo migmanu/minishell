81249f27cb3ad522e10f1c576dbcfc278ad0550d
migmanu <jmanuelmigoya@gmail.com>
2023-11-08 16:17:57 +0100

    Lay first approach to executor

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: migmanu <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 14:22:24 by migmanu           #+#    #+#             */
-/*   Updated: 2023/11/06 17:59:37 by migmanu          ###   ########.fr       */
+/*   Updated: 2023/11/08 15:46:33 by jmigoya-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef MINISHELL_H
@@ -22,6 +22,25 @@
 # define ERROR -1
 # define FAILURE 1
 # define SUCCESS 0
+
+typedef struct s_scmd
+{
+	char			*full_cmd;
+	char			*path;
+	// we might still need pointer to builtin function here
+	int				in_fd;
+	int				out_fd;
+	struct s_scmd	*next;
+}			t_scmd;
+
+typedef struct s_data
+{
+	char			*line; // store the line from readline()
+	char			**envp;
+	struct s_scmd	*scmds;
+	char			*pwd;
+	char			*old_pwd;
+}	t_data;
 
 // WARNING: test:
 void	init_promt(char *str);
--- src/executor/executor.c
+++ src/executor/executor.c
@@ -6,12 +6,53 @@
 /*   By: jmigoya- <jmigoya-@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 13:49:09 by jmigoya-          #+#    #+#             */
-/*   Updated: 2023/11/08 14:21:19 by jmigoya-         ###   ########.fr       */
+/*   Updated: 2023/11/08 15:51:29 by jmigoya-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
+#include <unistd.h>
+
+void	exec_smcds(t_data *data)
+{
+	char	**cmd;
+
+	cmd = ft_split(data->scmds->full_cmd, ' ');
+	execve(data->scmds->path, cmd, data->envp);
+	// handle error
+}
+
+void	dup_scmds(t_data *data, int fds[])
+{
+	// TODO: add builtin check
+	dup2(fds[1], STDOUT_FILENO);
+	close(fds[1]);
+	dup2(fds[0], STDIN_FILENO);
+	close(fds[0]);
+	exec_smcds(data);
+}
+
+void	fork_cmd(t_data *data, int fds[])
+{
+	int	pid;
+
+	pid = fork();
+	if (pid == -1)
+		return ; // TODO: handle error
+	if (pid == 0)
+	{
+		dup_scmds(data, fds);
+	}
+}
 
 void	executor_router(t_data *data)
 {
+	int	fds[2] = {STDIN_FILENO, STDOUT_FILENO};// fixed to stdin and stdout for now
+	if (pipe(fds) == -1)
+		return ; // TODO: handle error
+	while (data->scmds)
+	{
+		fork_cmd(data, fds);
+		data->scmds = data->scmds->next;
+	}
 }
