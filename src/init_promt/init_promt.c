eb6f35ef623b97e2f2f4d74719d408c89f5d9671
migmanu <jmanuelmigoya@gmail.com>
2023-11-10 17:01:22 +0100

    Move prompt to main funciton

--- src/init_promt/init_promt.c
+++ src/init_promt/init_promt.c
@@ -1,29 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   readline.c                                         :+:      :+:    :+:   */
+/*   init_promt.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jmigoya- <jmigoya-@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 16:43:12 by jmigoya-          #+#    #+#             */
-/*   Updated: 2023/11/10 16:50:54 by jmigoya-         ###   ########.fr       */
+/*   Updated: 2023/11/10 17:00:12 by jmigoya-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-char	*create_promt(t_data mish, char *str)
+char	*create_prompt(t_data mish, char *str)
 {
 	char	*symbol;
 	char	*var;
 	char	*prompt;
-	char	*line;
+	char	*color;
 
-	symbol = "\033[1;36m $_> \033[0;0m";
+	symbol = " $_> \033[0;0m";
 	var = hashmap_search(mish.env, str);
 	if (var)
 	{
-		prompt = ft_strjoin(var, symbol);
+		color = ft_strjoin("\033[1;36m", var);
+		prompt = ft_strjoin(color, symbol);
+		free(color);
 	}
 	else
 	{
@@ -34,5 +36,19 @@ char	*create_promt(t_data mish, char *str)
 
 char	*init_prompt(t_data *mish)
 {
+	char	*prompt;
+	char	*line;
 
+	prompt = create_prompt(*mish, "USER");
+	line = readline(prompt);
+	free(prompt);
+	if (line)
+	{
+		add_history(line);
+	}
+	else
+	{
+		exit(SUCCESS);
+	}
+	return (line);
 }
