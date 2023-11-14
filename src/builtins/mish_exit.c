60f65d2648089cc562183fea6832e968b78fc17a
migmanu <jmanuelmigoya@gmail.com>
2023-11-14 18:59:06 +0100

    Initial commit

---
+++ src/builtins/mish_exit.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mish_exit.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmigoya- <jmigoya-@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/14 12:08:19 by jmigoya-          #+#    #+#             */
+/*   Updated: 2023/11/14 18:57:59 by jmigoya-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+#include <unistd.h>
+
+void	mish_exit(t_data *mish, t_scmd cmd)
+{
+	printf("mish_exit init\n");
+	if (cmd.full_cmd[1] != NULL)
+	{
+		// handle error
+		ft_putstr_fd("exit command takes no input\n", STDERR_FILENO);
+		exit(ERROR);
+	}
+	handle_exit(mish, SUCCESS);
+	exit(ERROR); // handle_exit
+}
