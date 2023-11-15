7c9b6db79cd4dc82cebea50376f2b44851f32fdb
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2023-11-15 19:02:10 +0100

    comment some functions on the main to try to compile

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: migmanu <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 15:50:52 by migmanu           #+#    #+#             */
-/*   Updated: 2023/11/15 18:50:17 by sebasnadu        ###   ########.fr       */
+/*   Updated: 2023/11/15 19:00:29 by sebasnadu        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,17 +25,16 @@ int	main(int argc, char *argv[], char *envp[])
 		config_signals();
 		line = init_prompt(&mish);
 		input_handler(line, &mish);
-		//executor_router(&mish);
 		// TODO: this clean all the cmds and also close fds of files but, i think
 		// it should be provisional for now, this should be after the executor but inside
 		// your function to have more cleaning main
 		if (mish.cmds)
 			ft_lstclear(&mish.cmds, free_scmd);
-    if (line[0] == '\0')
-      continue;
+		// if (line[0] == '\0')
+			// continue ;
 		//input_handler(line, &mish);
-		create_mock_mish(&mish, line);
-		executor_router(&mish);
+		// create_mock_mish(&mish, line);
+		// executor_router(&mish);
 	}
 	return (0);
 }
