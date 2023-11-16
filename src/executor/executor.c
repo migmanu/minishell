c42cde60c21966b71e021693f728b41f520c8d60
Jose Manuel Migoya Bussolotti <jmanuelmigoya@gmai.com>
2023-11-16 16:52:53 +0100

    feat: get cmd path

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -6,7 +6,7 @@
 /*   By: jmigoya- <jmigoya-@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 13:49:09 by jmigoya-          #+#    #+#             */
-/*   Updated: 2023/11/15 17:24:55 by jmigoya-         ###   ########.fr       */
+/*   Updated: 2023/11/16 16:52:00 by jmigoya-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,40 @@
 
 extern char	**environ; // temp way of getting enviroment variables
 
-void	exec_s_cmds(t_data *mish, t_scmd *cmd)
+char	*get_path(t_data *mish, char *cmd)
 {
-	printf("exec_s_cmds init\n");
-	if (mish->pid)
-		mish->pid++;
+	char	**path_vec;
+	char	*tmp_path;
+	char	*path;
+	int		i;
+
+	i = 0;
+	path_vec = ft_split(hashmap_search(mish->env, "PATH") + 5, ':');
+	i = 0;
+	while (path_vec[i])
+	{
+		tmp_path = ft_strjoin("/", cmd);
+		path = ft_strjoin(path_vec[i], tmp_path);
+		free(tmp_path);
+		if (access(path, F_OK) == 0)
+			return (path);
+		free(path);
+		i++;
+	}
+	i = 0;
+	while (path_vec[i])
+		free(path_vec[i++]);
+	free(path_vec);
+	return (NULL);
+}
+
+void	exec_s_cmds(t_scmd *cmd)
+{
+	printf("exec_s_cmds init, path: %s\n", cmd->path);
 	execve(cmd->path, cmd->full_cmd, environ); // TODO: change environ!
 }
 
-void	dup_s_cmds(t_data *mish, t_scmd *cmd)
+void	dup_s_cmds(t_scmd *cmd)
 {
 	printf("dup_s_cmds init, in: %d | out: %d\n", cmd->in_fd, cmd->out_fd);
 	// TODO: add check for builtin cmds
@@ -37,7 +62,7 @@ void	dup_s_cmds(t_data *mish, t_scmd *cmd)
 		dup2(cmd->in_fd, STDIN_FILENO);
 		close(cmd->in_fd);
 	}
-	exec_s_cmds(mish, cmd);
+	exec_s_cmds(cmd);
 }
 
 void	fork_s_cmds(t_data *mish, t_scmd *cmd)
@@ -50,7 +75,7 @@ void	fork_s_cmds(t_data *mish, t_scmd *cmd)
 		return ; // TODO: handle error
 	if (pid == 0)
 	{
-		dup_s_cmds(mish, cmd);
+		dup_s_cmds(cmd);
 	}
 	else
 	{
@@ -67,7 +92,9 @@ void	executor_router(t_data *mish)
 	printf("executor_router init\n");
 	while (mish->cmds)
 	{
+		printf("while init\n");
 		cmd = mish->cmds->content;
+		cmd->path = get_path(mish, cmd->full_cmd[0]);
 		if (mish->cmds->next != NULL)
 		{
 			next_cmd = mish->cmds->next->content;
