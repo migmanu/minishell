20b72f2434e9f23860b8ce1f613217b6e26d8ed1
Jose Manuel Migoya Bussolotti <jmanuelmigoya@gmai.com>
2023-11-17 16:35:28 +0100

    feat: initial commit

---
+++ src/builtins/mish_export.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mish_export.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmigoya- <jmigoya-@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/17 13:25:59 by jmigoya-          #+#    #+#             */
+/*   Updated: 2023/11/17 14:28:58 by jmigoya-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+/*
+void	mish_export(t_data *mish, t_scmd cmd)
+{
+	// TODO: check, is there a way to just get the exported vars from the
+	// hash map? Needed for export with no arguments
+}*/
