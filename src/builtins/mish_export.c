e6992ecb0fcd7d901cc6d324e7860d2a3ea0953d
migmanu <jmanuelmigoya@gmail.com>
2023-11-21 22:39:48 +0100

    feat: move ft_chr_pos to builtins_router.c

--- src/builtins/mish_export.c
+++ src/builtins/mish_export.c
@@ -6,34 +6,15 @@
 /*   By: jmigoya- <jmigoya-@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 13:25:59 by jmigoya-          #+#    #+#             */
-/*   Updated: 2023/11/21 16:47:56 by jmigoya-         ###   ########.fr       */
+/*   Updated: 2023/11/21 22:19:00 by jmigoya-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-// Returns the index of the first occurance of c 
-// in string str. Returns -1 if no c or error
-int	ft_chr_pos(const char *str, const char c)
-{
-	int	i;
-
-	i = 0;
-	if (!str)
-		return (-1);
-	while (str[i] != '\0')
-	{
-		if (str[i] == c)
-		{
-			return (i);
-		}
-		i++;
-	}
-	return (-1);
-}
-
 // TODO: check, is there a way to just get the exported vars from the
 // hash map? Needed for export with no arguments
+// TODO: check if more than one var per command
 void	mish_export(t_data *mish, t_scmd cmd, int if_exit)
 {
 	printf("export init\n");
