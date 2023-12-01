2842d2004794f42d87369d33d515779349d77893
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2023-12-01 18:08:11 +0100

    Add handler invalid pipe in redirections

--- src/input_handler/redirections.c
+++ src/input_handler/redirections.c
@@ -6,7 +6,7 @@
 /*   By: sebasnadu <johnavar@student.42berlin.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 17:57:07 by sebasnadu         #+#    #+#             */
-/*   Updated: 2023/12/01 16:57:06 by sebasnadu        ###   ########.fr       */
+/*   Updated: 2023/12/01 18:06:37 by sebasnadu        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,15 @@ t_scmd	*redir_out_append(t_scmd *node, char **cmds, int *i)
 	flags[0] = 1;
 	flags[1] = 1;
 	(*i)++;
-	if (cmds[++(*i)] && cmds[*i][0] != '>')
+	if (cmds[++(*i)] && (cmds[*i][0] != '>' && cmds[*i][0] != '|'))
 		node->out_fd = get_fd(node->out_fd, cmds[*i], flags);
-	if (!cmds[*i] || cmds[*i][0] == '>' || node->out_fd == -1)
+	if (!cmds[*i] || cmds[*i][0] == '>' || cmds[*i][0] == '|'
+		|| node->out_fd == -1)
 	{
 		if (cmds[*i] && cmds[*i][0] == '>')
 			handle_exit(NULL, "`>>'", SYNTAX_ERR, NOT_EXIT);
+		else if (cmds[*i] && cmds[*i][0] == '|')
+			handle_exit(NULL, "`|'", SYNTAX_ERR, NOT_EXIT);
 		else if (node->out_fd != -1)
 			handle_exit(NULL, "`newline'", SYNTAX_ERR, NOT_EXIT);
 		*i = -1;
@@ -39,13 +42,14 @@ t_scmd	*redir_out(t_scmd *node, char **cmds, int *i)
 	flags[0] = 1;
 	flags[1] = 0;
 	(*i)++;
-	if (cmds[*i] && cmds[*i][0] != '<')
+	if (cmds[*i] && (cmds[*i][0] != '<' && cmds[*i][0] != '|'))
 		node->out_fd = get_fd(node->out_fd, cmds[*i], flags);
-	if (!cmds[*i] || cmds[*i][0] == '<' || node->out_fd == -1)
+	if (!cmds[*i] || cmds[*i][0] == '<' || cmds[*i][0] == '|'
+		|| node->out_fd == -1)
 	{
 		if (cmds[*i] && cmds[*i][0] == '<')
 			handle_exit(NULL, "`<'", SYNTAX_ERR, NOT_EXIT);
-		else if (node->out_fd != -1)
+		else if (node->out_fd != -1 || (cmds[*i] && cmds [*i][0] == '|'))
 			handle_exit(NULL, "`newline'", SYNTAX_ERR, NOT_EXIT);
 		else
 			g_exit_status = 1;
@@ -60,15 +64,18 @@ t_scmd	*redir_in_heredoc(t_scmd *node, char **cmds, int *i)
 
 	limit = NULL;
 	(*i)++;
-	if (cmds[++(*i)] && cmds[*i][0] != '<')
+	if (cmds[++(*i)] && (cmds[*i][0] != '<' && cmds[*i][0] != '|'))
 	{
 		limit = cmds[*i];
 		node->in_fd = get_heredoc_fd(limit);
 	}
-	if (!cmds[*i] || cmds[*i][0] == '<' || node->in_fd == -1)
+	if (!cmds[*i] || cmds[*i][0] == '<' || cmds[*i][0] == '|'
+		|| node->in_fd == -1)
 	{
 		if (cmds[*i] && cmds[*i][0] == '<')
 			handle_exit(NULL, "`<<'", SYNTAX_ERR, NOT_EXIT);
+		else if (cmds[*i] && cmds[*i][0] == '|')
+			handle_exit(NULL, "`|'", SYNTAX_ERR, NOT_EXIT);
 		else if (node->in_fd != -1)
 			handle_exit(NULL, "`newline'", SYNTAX_ERR, NOT_EXIT);
 		*i = -1;
@@ -83,11 +90,13 @@ t_scmd	*redir_in(t_scmd *node, char **cmds, int *i)
 	flags[0] = 0;
 	flags[1] = 0;
 	(*i)++;
-	if (cmds[*i] && cmds[*i][0] != '>')
+	if (cmds[*i] && (cmds[*i][0] != '>' && cmds[*i][0] != '|'))
 		node->in_fd = get_fd(node->in_fd, cmds[*i], flags);
-	if (!cmds[*i] || cmds[*i][0] == '>' || node->in_fd == -1)
+	if (!cmds[*i] || cmds[*i][0] == '>' || cmds[*i][0] == '|'
+		|| node->in_fd == -1)
 	{
-		if (node->in_fd != -1 || (cmds[*i] && cmds[*i][0] == '>'))
+		if (node->in_fd != -1 || (cmds[*i]
+				&& (cmds[*i][0] == '>' || cmds[*i][0] == '|')))
 			handle_exit(NULL, "`newline'", SYNTAX_ERR, NOT_EXIT);
 		else
 			g_exit_status = 1;
