c8b437f75d740784c96dca6ee3146716353005e3
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2023-12-06 21:37:34 +0100

    merge

--- src/signals/signals.c
+++ src/signals/signals.c
@@ -6,12 +6,12 @@
 /*   By: migmanu <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 15:22:11 by migmanu           #+#    #+#             */
-/*   Updated: 2023/12/06 19:38:11 by jmigoya-         ###   ########.fr       */
+/*   Updated: 2023/12/06 21:30:33 by sebasnadu        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
-#include <asm-generic/ioctls.h>
+// #include <asm-generic/ioctls.h>
 #include <readline/readline.h>
 #include <unistd.h>
 
