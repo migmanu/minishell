70fcd1ad5b9d53a422ba0c37e1a0ccf1480ace05
Jose Manuel Migoya Bussolotti <jmanuelmigoya@gmai.com>
2023-12-06 16:14:20 +0100

    fix: fix flag check value

--- src/builtins/mish_echo.c
+++ src/builtins/mish_echo.c
@@ -6,7 +6,7 @@
 /*   By: jmigoya- <jmigoya-@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 18:58:49 by jmigoya-          #+#    #+#             */
-/*   Updated: 2023/12/06 12:14:48 by jmigoya-         ###   ########.fr       */
+/*   Updated: 2023/12/06 16:12:45 by jmigoya-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ static int	check_flag(char *str)
 	if (!str)
 		return (-1);
 	if (str[0] != '-' || str[1] != 'n')
-		return (1);
+		return (0);
 	i = 1;
 	while (str[i] != '\0')
 	{
@@ -45,7 +45,7 @@ void	mish_echo(t_data *mish, t_scmd cmd, int if_exit)
 	f = 0;
 	while (cmd.full_cmd[i] != NULL)
 	{
-		if (check_flag(cmd.full_cmd[i]) != 1 || f == 1)
+		if (check_flag(cmd.full_cmd[i]) == 0 || f == 1)
 		{
 			f = 1;
 			ft_putstr_fd(cmd.full_cmd[i], cmd.out_fd);
@@ -54,7 +54,7 @@ void	mish_echo(t_data *mish, t_scmd cmd, int if_exit)
 		}
 		i++;
 	}
-	if (check_flag(cmd.full_cmd[1]) != 0)
+	if (check_flag(cmd.full_cmd[1]) == 0)
 		ft_putstr_fd("\n", cmd.out_fd);
 	handle_exit(mish, NULL, SUCCESS, if_exit);
 }
