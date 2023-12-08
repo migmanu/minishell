6347343776f85c2a40fccb870ae52cf7e30cfa2f
Johan Sebastian Navarro Duarte <johnavar@c4a10c7.42berlin.de>
2023-12-08 14:30:48 +0100

    push main

--- src/mish/main.c
+++ src/mish/main.c
@@ -6,7 +6,7 @@
 /*   By: jmigoya- <jmigoya-@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 15:50:52 by migmanu           #+#    #+#             */
-/*   Updated: 2023/12/08 14:22:02 by jmigoya-         ###   ########.fr       */
+/*   Updated: 2023/12/08 14:30:22 by johnavar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ int	main(int argc, char *argv[], char *envp[])
 		input_handler(line, &mish);
 		executor(&mish);
 		if (g_signal == SIGINT)
-			g_exit_status = 128 + g_signal;
+			mish.exit_status = 128 + g_signal;
 	}
 	handle_exit(&mish, NULL, SUCCESS, IS_EXIT);
 	return (0);
